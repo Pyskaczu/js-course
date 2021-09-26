534db1e5ee833de602ff0376185455f9f5378665
Michal Babiarz <michal.babiarz84@gmail.com>
2021-09-26 21:26:54 +0200

    implements buffered rollover

---
+++ rollover-buffering/rollover-buffering.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", evt => {
+    rolloverInit()
+});
+
+function rolloverInit() {
+    for (let i = 0; i < document.images.length; i++) {
+        if(document.images[i].parentNode.tagName === "A") {
+            setupRollover(document.images[i]);
+        }
+    }
+}
+
+function setupRollover(image) {
+    image.outImage = new Image();
+    image.outImage.src = image.src;
+    image.addEventListener("mouseout", evt => {
+        image.src = image.outImage.src;
+        // OR evt.target.src = evt.target.outImage.src;
+    });
+
+    image.overImage = new Image();
+    image.overImage.src = "images/" + image.id + "_on.png";
+    image.addEventListener("mouseover", evt => {
+        // or image.src = image.overImage.src;
+        evt.target.src = evt.target.overImage.src;
+    });
+}
