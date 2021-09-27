96bb36a69aa16653d867db2b8ed7aa6fe62f7541
Michal Babiarz <michal.babiarz84@gmail.com>
2021-09-27 17:47:58 +0200

    Implements Leonardo Rollover

--- rollover-leonardo/rollover-leonardo.js
+++ rollover-leonardo/rollover-leonardo.js
@@ -1,12 +1,12 @@
 window.addEventListener("load", evt => {
-    rolloverInit()
+    rolloverInit();
 });
 
 function rolloverInit() {
     for (let i = 0; i < document.links.length; i++) {
         let linkObj = document.links[i];
-        if (linkObj.id) {
-            let imgObj = document.getElementById(linkObj.id + "Img");
+        if (linkObj.className) {
+            let imgObj = document.getElementById(linkObj.className);
             if (imgObj) {
                 setupRollover(linkObj, imgObj);
             }
@@ -21,10 +21,10 @@ function setupRollover(linkObj, imgObj) {
     linkObj.outImage.src = imgObj.src;
 
     linkObj.overImage = new Image();
-    linkObj.overImage.src = "images/" + linkObj.id + "_on.png";
+    linkObj.overImage.src = "images/" + linkObj.id + "Text.gif";
 
     linkObj.addEventListener("mouseover", evt => {
-        evt.target.imageToChange.src = evt.target.overImage.src;
+        evt.target.parentNode.imageToChange.src = evt.target.parentNode.overImage.src;
     });
 
     linkObj.addEventListener("mouseout", evt => {
