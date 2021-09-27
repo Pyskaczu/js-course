ffc4eaaf7cb4df5c808a18e6df53e94434ee22ed
Michal Babiarz <michal.babiarz84@gmail.com>
2021-09-27 22:54:20 +0200

    adds links to banner

--- banner/banner.js
+++ banner/banner.js
@@ -1,12 +1,29 @@
-window.addEventListener("load", ev => {rotate();});
+window.addEventListener("load", evt => {initBanner();});
 
 let thisAd = 0;
 const imageBase = "images/reading";
+
+function initBanner() {
+   document.getElementById("adBanner").parentNode.addEventListener("click", evt => {goToLink();});
+   rotate();
+}
+
+function goToLink() {
+   let links = ["google.com", "helion.pl", "gmail.com"];
+   document.location.href = "http://www." + links[thisAd];
+
+   //workaround for non working return false
+   let count = 1000000000;
+   while(--count){
+   }
+
+   return false;
+}
 
 function rotate() {
    document.getElementById("adBanner").src = imageBase + thisAd + ".gif";
    thisAd++;
    thisAd %= 3;
 
-   setTimeout(rotate, 3*1000);
+   setTimeout(rotate, 1000);
 }
