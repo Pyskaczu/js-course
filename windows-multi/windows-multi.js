479065f172fda62e53d6b9ec24d2d04786a539e0
Michal Babiarz <michal.babiarz84@gmail.com>
2021-09-29 23:04:32 +0200

    Implements wondows-multi

--- windows-multi/windows-multi.js
+++ windows-multi/windows-multi.js
@@ -6,11 +6,10 @@ function initWinLinks() {
     for(let i = 0; i < document.links.length - 1; i++)
     document.links[i].addEventListener("click", ev => {
         ev.preventDefault();
-        openNewWindow(ev.target);
+        openNewWindow(ev.target, i);
     });
 }
 
-function openNewWindow(linkObj) {
-    let catWin = window.open(linkObj.href, "catWin", "location=true,toolbar=true,resizable=no");
-    catWin.focus();
+function openNewWindow(linkObj, id) {
+    let catWin = window.open(linkObj.href, "catWin" + id, "location=true,toolbar=true,resizable=no");
 }
